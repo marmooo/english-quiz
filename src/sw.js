67b509dac89e01cff58ac155010858a6290ee339
marmooo <marmooo@users.noreply.github.com>
2022-07-19 00:47:37 +0900

    bump signature_pad from 4.0.5 to 4.0.6

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-07-19 00:32";
+var CACHE_NAME = "2022-07-19 00:47";
 var urlsToCache = [
   "/english-quiz/",
   "/english-quiz/data/0.tsv",
@@ -17,7 +17,7 @@ var urlsToCache = [
   "/english-quiz/mp3/correct3.mp3",
   "/english-quiz/favicon/favicon.svg",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
-  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.5/dist/signature_pad.umd.min.js",
+  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.6/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.17.0/dist/tf.min.js",
   "https://cdn.jsdelivr.net/npm/animate.css@4.1.1/animate.min.css",
 ];

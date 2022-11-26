52ca481238bf8bf2e0ed08a1955ae1a6707781c6
marmooo <marmooo@users.noreply.github.com>
2022-11-26 13:00:49 +0900

    bump bootstrap from 5.2.2 to 5.2.3

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-11-17 08:05";
+var CACHE_NAME = "2022-11-26 12:58";
 var urlsToCache = [
   "/english-quiz/",
   "/english-quiz/data/0.tsv",
@@ -16,7 +16,7 @@ var urlsToCache = [
   "/english-quiz/mp3/end.mp3",
   "/english-quiz/mp3/correct3.mp3",
   "/english-quiz/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/signature_pad@4.1.1/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.0.0/dist/tf.min.js",
   "https://cdn.jsdelivr.net/npm/animate.css@4.1.1/animate.min.css",

6e1f5001668038c62394d3e74cd4c444ad69f001
marmooo <marmooo@users.noreply.github.com>
2024-03-20 09:48:47 +0900

    use esm

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "2024-03-07 01:30";
+const CACHE_NAME = "2024-03-20 09:50";
 const urlsToCache = [
   "/english-quiz/",
   "/english-quiz/index.js",
@@ -16,7 +16,6 @@ const urlsToCache = [
   "/english-quiz/mp3/correct3.mp3",
   "/english-quiz/img/surfing-js.png",
   "/english-quiz/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/signature_pad@4.1.7/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.16.0/dist/tf.min.js",
   "https://cdn.jsdelivr.net/npm/animate.css@4.1.1/animate.min.css",
 ];

017cb3e654108055410a8f9ed24c6f006b93f2e4
marmooo <marmooo@users.noreply.github.com>
2026-05-27 11:25:15 +0900

    add favicon.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,3 @@
-tmpSvg=$(mktemp --suffix=.svg)
-inkscape src/favicon/favicon.svg \
-  --export-text-to-path \
-  --export-plain-svg \
-  --export-filename="${tmpSvg}"
-resvg -w 48 -h 48 "${tmpSvg}" src/favicon/favicon.png
-rm -f "${tmpSvg}"
-
 mkdir -p docs
 cp -r src/* docs
 drop-inline-css -r src -o docs
